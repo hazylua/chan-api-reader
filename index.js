ecb8afc7ccf6c51025b197900789d27acd9c00cb
yb00 <40061395+yb00@users.noreply.github.com>
2020-09-06 18:52:35 -0300

    Reformat index.js

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const path = require("path")
 
 const config = require("./config/server")
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .get('/', (req, res) => res.render('pages/index'))
-    .listen(config.port, () => console.log(`Listening on port ${config.port}.`))
+const app = express()
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+app.listen(config.port, () => console.log(`Listening on port ${config.port}.`))
